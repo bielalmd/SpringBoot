1e100883dad6a07fb08fb10315296ad2c9866401
Gabriel Almeida <86261232+bielalmd@users.noreply.github.com>
2021-07-23 13:16:37 -0300

    primeira atividade de springboot

---
+++ helloworld/src/main/java/br/org/generation/helloworld/contoller/Atividade2.java
@@ -0,0 +1,16 @@
+package br.org.generation.helloworld.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/atividade2")
+public class Atividade2 {
+	
+	@GetMapping
+	public String atividade2() {
+		return " Meus objetivos para essa semana é focar no processo de aprendizagem e em persistir no conteudo!";
+	}
+
+}
---
+++ helloworld/src/main/java/br/org/generation/helloworld/contoller/HelloController.java
@@ -0,0 +1,16 @@
+package br.org.generation.helloworld.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public String hello() {
+		return "Habilidades de atenção ao detalhe e persistencia!";
+	}
+
+}
