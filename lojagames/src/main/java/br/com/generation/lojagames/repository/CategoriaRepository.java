0760ac8079b1306b1613c6bff82870733c0bceb2
Gabriel Almeida <86261232+bielalmd@users.noreply.github.com>
2021-07-30 23:16:04 -0300

    exercicio loja de games

---
+++ lojagames/src/main/java/br/com/generation/lojagames/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package br.com.generation.lojagames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.generation.lojagames.model.CategoriaModel;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+	public List<CategoriaModel> findAllByTipoContainingIgnoreCase(CategoriaModel categoria);
+}
